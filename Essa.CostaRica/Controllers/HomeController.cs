f42044d13686980a0b1fd1fe6b87c9e0039dfdb5
Pablo Rodriguez Apú <prodriguez@gpilatam.com>
2017-03-25 07:59:44 -0600

    Adding solution to Git

---
+++ Essa.CostaRica/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace Essa.CostaRica.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string[] filePaths = Directory.GetFiles(Server.MapPath(ConfigurationManager.AppSettings["CarouselPath"]));
+            List<string> list = new List<string>();
+            foreach (string item in filePaths)
+            {
+                list.Add(ConfigurationManager.AppSettings["CarouselPath"].Replace("\\", "/") + "/" + Path.GetFileName(item));
+            }
+            ViewBag.CarouselPaths = list;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Essa.CostaRica/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Essa.CostaRica.MvcApplication" Language="C#" %>
